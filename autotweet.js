8d8f795ca106fbb9a6376565fa0c5d2a24315210
Javed Nissar <javednissar@gmail.com>
2014-03-02 14:21:25 -0500

    working on the Markov Chain class

--- autotweet.js
+++ autotweet.js
@@ -1,13 +1,36 @@
 var twitter=require('twitter');
 var fs=require('fs');
 
-function Generator(corpus){
-	this.transitionMatrix={}
-	this.corpus=corpus;
+function MarkovChainGenerator(corpus){
+	this.transitionMatrix={};
+	this.collection={}
+	for(var x=0;x<corpus.length;x++){
+		var sentenceTokens=tokenize(corpus[x]);
+		for(var y=0;y<sentenceTokens.length;y++){
+			if(y!=sentenceTokens.length-1){
+				token=sentenceTokens[y];
+				if(this.collection[token]===undefined){
+					this.collection[token]=[];
+				}
+				this.collection[token].push(sentenceTokens[y+1]);
+			}
+		}
+	}
+	for(var token in this.collection){
+		var occurences=this.collection[token];
+		this.transitionMatrix[token]={};
+		for(var i=0;i<occurences.length;i++){
+			occurence=occurences[i];
+			if(this.transitionMatrix[token][occurence]===undefined){
+				this.transitionMatrix[token][occurence]=1;
+			}else{
+				this.transitionMatrix[token][occurence]++;
+			}
+		}
+	}
 }
-Generator.prototype={
 
-}
+
 //tokenize a string by splitting it by the spaces
 function tokenize(string){
 	return string.split(" ");
@@ -24,11 +47,8 @@ function readSettings(err,fd){
 			access_token_key:settings.access_token_key,
 			access_token_secret:settings.access_token_secret
 		});
-		var corpus=settings.corpus;
-		var generator=new Generator(corpus);
-		for(var x=0;x<corpus.length;x++){
-			var sentenceTokens=tokenize(corpus[x]);
-		}
+		var generator=new MarkovChainGenerator(settings.corpus);
+		console.log(generator.transitionMatrix);
 	};
 }
 fs.readFile("./settings.json",'utf8',readSettings);
