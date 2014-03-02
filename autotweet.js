639970e2d4d995b9c51aa94639e435bca087cda7
Javed Nissar <javednissar@gmail.com>
2014-03-02 15:27:56 -0500

    added a function for generating words, a function for generating random integers, and made some style changes

--- autotweet.js
+++ autotweet.js
@@ -1,6 +1,12 @@
 var twitter=require('twitter');
 var fs=require('fs');
 
+// Returns a random integer between min and max
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function MarkovChainGenerator(corpus){
 	this.transitionMatrix={};
 	numbers={};
@@ -8,8 +14,8 @@ function MarkovChainGenerator(corpus){
 	for(var x=0;x<corpus.length;x++){
 		var sentenceTokens=corpus[x].split(" ");
 		for(var y=0;y<sentenceTokens.length;y++){
-			if(y!=sentenceTokens.length-1){
-				token=sentenceTokens[y];
+			if(y!==sentenceTokens.length-1){
+				var token=sentenceTokens[y];
 				if(collection[token]===undefined){
 					collection[token]=[];
 				}
@@ -32,12 +38,33 @@ function MarkovChainGenerator(corpus){
 	for(var token in numbers){
 		this.transitionMatrix[token]={}
 		total=Object.keys(numbers[token]).length;
-		for(var possibility in numbers[token]){
-			this.transitionMatrix[token][possibility]=numbers[token][possibility]/total;
+		for(var state in numbers[token]){
+			this.transitionMatrix[token][state]=numbers[token][state]/total;
 		}
 	}
 }
-
+MarkovChainGenerator.prototype={
+	generateWord:function(token){
+		var possibilities={};
+		for(var possibility in this.transitionMatrix[token]){
+			possibilities[possibility]={"probability":0,"complete":false};
+			possibilities[possibility]["probability"]=Math.round(this.transitionMatrix[token][possibility]*100);
+		}
+		var array=[];
+		currentToken=0;
+		for(var i=0;i<100;i++){
+			var token=(Object.keys(possibilities)[currentToken]);
+			if(i>possibilities[token]["probability"]){
+				currentToken++;
+				token=(Object.keys(possibilities)[currentToken]);
+				array.push(token);
+			}else{
+				array.push(token);
+			}
+		}
+		return array[getRandomInt(0,array.length)];
+	}
+}
 //read the settings.json file
 function readSettings(err,fd){
 	if(err){
@@ -51,7 +78,7 @@ function readSettings(err,fd){
 			access_token_secret:settings.access_token_secret
 		});
 		var generator=new MarkovChainGenerator(settings.corpus);
-		console.log(generator.transitionMatrix);
+		console.log(generator.generateWord("Waldo"),"generated word");
 	};
 }
 fs.readFile("./settings.json",'utf8',readSettings);
